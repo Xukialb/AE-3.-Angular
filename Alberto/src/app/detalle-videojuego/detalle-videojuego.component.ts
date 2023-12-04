e744c8dfdb3ae301ccd5eff28b3f6e239376e952
Alberto <albertosaboya@gmail.com>
2023-12-04 11:17:04 +0100

    docReq1

--- Alberto/src/app/detalle-videojuego/detalle-videojuego.component.ts
+++ Alberto/src/app/detalle-videojuego/detalle-videojuego.component.ts
@@ -7,60 +7,68 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./detalle-videojuego.component.css']
 })
 export class DetalleVideojuegoComponent {
+  // Declaración de variables de clase
   videojuegoId: number;
-  videojuego: any; // Puedes usar un modelo si prefieres
+  videojuego: any;
 
+  // Constructor del componente que recibe una instancia de ActivatedRoute
   constructor(private route: ActivatedRoute) {
+    // Inicialización de la variable videojuegoId con un valor predeterminado
     this.videojuegoId = 0;
+
+    // Suscripción a los cambios en los parámetros de la ruta
     this.route.params.subscribe(params => {
+      // Actualización de videojuegoId con el valor proporcionado en los parámetros de la ruta
       this.videojuegoId = +params['id'];
+
+      // Llamada a la función para cargar los detalles del videojuego
       this.cargarDetalleVideojuego(this.videojuegoId);
-      // Aquí cargarías los detalles del videojuego directamente, por ejemplo:  
     });
   }
+
+  // Función para cargar los detalles del videojuego
   cargarDetalleVideojuego(id: number) {
-    // Aquí deberías implementar la lógica para cargar el videojuego según el ID
-    // Por ahora, se proporciona un ejemplo con datos estáticos
+    // Condicionales para determinar qué detalles cargar basados en el id proporcionado
     if (id === 1) {
-    this.videojuego = {
-      id: id,
-      titulo: 'Zelda Breath of the Wild',
-      compañia: 'Nintendo',
-      imagen: 'assets/imagenes/zbotw.jpg',
-      valoracion: 4.5
-    };
-  } else if (id === 2) {
-    this.videojuego = {
-      id: id,
-      titulo: 'The Last of Us',
-      compañia: 'Naughty Dog',
-      imagen: 'assets/imagenes/tlou.jpg',
-      valoracion: 4.8
-    };
-  } else if (id === 3) {
-    this.videojuego = {
-      id: id,
-      titulo: 'Marvels Spider-man',
-      compañia: 'Insoniac Games',
-      imagen: 'assets/imagenes/spiderman.jpg',
-      valoracion: 4.5
-    };
-  } else if (id === 4) {
-    this.videojuego = {
-      id: id,
-      titulo: 'Microsoft Flight Simulator',
-      compañia: 'Microsoft Games',
-      imagen: 'assets/imagenes/mfs.jpg',
-      valoracion: 4.8
-    };
-  } else if (id === 5) {
-    this.videojuego = {
-      id: id,
-      titulo: 'Red Dead Redemption II',
-      compañia: 'Rockstar Games',
-      imagen: 'assets/imagenes/rdd2.jpg',
-      valoracion: 4.5
-    };
+      this.videojuego = {
+        id: id,
+        titulo: 'Zelda Breath of the Wild',
+        compañia: 'Nintendo',
+        imagen: 'assets/imagenes/zbotw.jpg',
+        valoracion: 4.5
+      };
+    } else if (id === 2) {
+      this.videojuego = {
+        id: id,
+        titulo: 'The Last of Us',
+        compañia: 'Naughty Dog',
+        imagen: 'assets/imagenes/tlou.jpg',
+        valoracion: 4.8
+      };
+    } else if (id === 3) {
+      this.videojuego = {
+        id: id,
+        titulo: 'Marvels Spider-man',
+        compañia: 'Imsoniac Games',
+        imagen: 'assets/imagenes/spiderman.jpg',
+        valoracion: 4.5
+      };
+    } else if (id === 4) {
+      this.videojuego = {
+        id: id,
+        titulo: 'Microsoft Flight Simulator',
+        compañia: 'Microsoft Games',
+        imagen: 'assets/imagenes/mfs.jpg',
+        valoracion: 4.8
+      };
+    } else if (id === 5) {
+      this.videojuego = {
+        id: id,
+        titulo: 'Red Dead Redemption II',
+        compañia: 'Rockstar Games',
+        imagen: 'assets/imagenes/rdd2.jpg',
+        valoracion: 4.5
+      };
+    }
   }
-}
 }
