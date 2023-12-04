501c24fe2630a431fcd3d6fe916bb79ef50ea828
Alberto <albertosaboya@gmail.com>
2023-12-04 20:26:26 +0100

    ultimo ajuste

--- GRUPO04/src/app/login/login.component.ts
+++ GRUPO04/src/app/login/login.component.ts
@@ -47,7 +47,6 @@ passwordIncorrecto=true
     else 
       // Mostramos el mensaje de login inválido
       console.log('Login inválido');
-      alert("No se ha podido realizar el login (usuario y/o contraseña incorrectos)");
       this.ocultarMensajeError(form)
      
     }
