b0cf1bb2ae48dceb3757dc4c23c28342ad033115
Anabella <anabella.aceto@gmail.com>
2023-12-04 12:13:55 +0100

    finalizado

--- GRUPO04/src/app/login/login.component.ts
+++ GRUPO04/src/app/login/login.component.ts
@@ -9,26 +9,68 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  
-  constructor(private router : Router) {}  
+
+ usuario: string= ""
+
+ //Definimos algunos usuarios y contraseñas válidos
+ usuariosValidos = [
+  { username: 'marta', password: '123' },
+  { username: 'pedro', password: '456' },
+  { username: 'juan', password: '789' }
+];
+
+usuarioObligatorioOculto = true
+passwordObligatorioOculto = true
+
+
+ constructor(private router : Router) {}  
 
   ngOnInit() {
   }
 
-
-login(form: NgForm): any {
+//Método que porcesa los datos ingresados en el formulario
+  login(form: any): void {
     const user = form.value.user;
     const password = form.value.password;
 
-    if (user && password) {
-    //Mostramos mensaje de login exitoso
-    console.log('Login exitoso');
-    this.router.navigate(['/lista']);
+    // Verificar si el usuario y la contraseña coinciden con los válidos
+    const usuarioValido = this.usuariosValidos.find(u => u.username === user && u.password === password);
 
-    } else {
+    if (usuarioValido) {
+      // Mostramos mensaje de login exitoso
+      console.log('Login exitoso');
+      this.usuario = user;
+      this.router.navigate(['/lista', { usuario: this.usuario }]);
+    } else 
       // Mostramos el mensaje de login inválido
       console.log('Login inválido');
+      this.ocultarMensajeError(form)
+     
+    }
+
+
+    public ocultarMensajeError(form: any) {
+      // Oculta el mensaje de error para todos los campos
+      this.usuarioObligatorioOculto = true;
+      this.passwordObligatorioOculto = true;
+  
+      // Muestra el mensaje de error solo si no se completa el usuario
+      if (!form.value.user) {
+        this.usuarioObligatorioOculto = false;
+      }
+      // Muestra el mensaje de error solo si no se completa el password
+      if (!form.value.password) {
+        this.passwordObligatorioOculto = false;
+      }
     }
   }
-}
 
+  
+
+   
+
+
+
+  
+   
+  
